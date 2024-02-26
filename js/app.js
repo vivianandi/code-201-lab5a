1827350589f2ea3e4d71777699207419af6ce36e
Vivian Davila <viviananddav@gmail.com>
2024-02-25 22:18:56 -0800

    Implement sum and multiplication function for problem3

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   let answer = a + b;
-  let result = [11, "The sum of 4 and 7 is 11."];
+  let result = [answer, `The sum of ${a} and ${b} is ${answer}.`];
   return result;
 }
 
@@ -51,11 +51,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sumNumbers = sum(sum(a, b)[0], c)[0];
+  let multiplyNumbers = multiply(a, b)[0] * c;
+  let sumString = `${a} and ${b} and ${c} sum to ${sumNumbers}.`;
+  let multiplyString = `The product of ${a} and ${b} and ${c} is ${multiplyNumbers}.`;
+  return [sumNumbers, multiplyNumbers, sumString, multiplyString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
